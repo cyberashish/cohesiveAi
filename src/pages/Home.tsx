1f727aebcdf65d11ed4bf3f4e564d0c85f074110
wrappixelTeam <cyberashish321@gmail.com>
2025-05-25 18:04:30 +0530

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import FeaturesSection from "@/components/home/FeaturesSection"
 import HeroSection from "@/components/home/HeroSection"
 import PricingSection from "@/components/home/PricingSection"
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const Home = () => {
-  AOS.init();
+
   return (
     <>
       <HeroSection/>
